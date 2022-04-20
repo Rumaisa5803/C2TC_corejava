d2b9a550ee65aed4c3f5f524e6d5f352942f081b
Rumaisa <abdullahkazi5803@gmail.com>
2022-04-20 17:27:32 +0530

    First Commit

---
+++ Java_Programs/src/nandu/claobj.java
@@ -0,0 +1,32 @@
+package nandu;
+
+public class claobj {
+    int yearofpurchase;// data members
+    String model;//data members
+    double cost;//data members
+   
+    
+    public void disp1()// Member function
+    {
+    	System.out.println("year of purchase"+2001);
+    
+    	
+    }
+public void disp2()
+{
+    System.out.println("model"+1999);
+    System.out.println("cost"+200000);
+}
+    public static void main(String[] argo) {
+    	claobj hai=new claobj(); 
+    	claobj bye=new claobj(); 
+    	hai.disp1();
+    	bye.disp2();
+    	// TODO Auto-generated method stub
+    	
+    }
+    
+    
+    
+    
+}    
